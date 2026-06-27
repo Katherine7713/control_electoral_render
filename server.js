eb2a4507e7a5a97772bf952188dcbf2a5342bceb
Katherine7713 <katherine.sailema@epn.edu.ec>
2026-06-27 18:16:34 -0500

    feat: add CORS support

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 require('dotenv').config();
+const cors = require('cors');
 const express = require('express');
 const path = require('path');
 
 const app = express();
+app.use(cors());
 const PORT = process.env.PORT || 3000;
 
 app.use(express.static(path.join(__dirname, 'public')));
